b05611ad26fb8a2abf49f58f053b14829e8f05bb
Richard <rjk6qe@virginia.edu>
2017-03-01 13:52:35 -0500

    Added a few more api calls

--- python/congress_api.py
+++ python/congress_api.py
@@ -17,6 +17,9 @@ def get_specific_member(member_id=None):
 	else:
 		return None
 
+def get_committees(congress=None, chamber=None):
+	return get_request((str(congress),chamber, 'committees'))
+
 def get_current_members_by_district(chamber=None, state=None, district=None):
 	#Chamber: house or senate, default to senate
 	#State: two-letters, default to VA, duh
@@ -57,7 +60,79 @@ def get_specific_roll_call_vote(congress=None, chamber=None, session_number=None
 	return get_request((str(congress), chamber, 'sessions', session_number, 'votes', roll_call_number))
 
 
-if __name__ == "__main__":
-	list_of_members = get_list_of_members()[0]['members']
+def congressperson_info():
+	request_data = {
+		'table':'Congressperson', 
+		'data':{
+			'senate':[],
+			'house':[]
+			}
+		}
 
-	print(post_request('api.php', list_of_members))
+	list_of_house_members = get_list_of_members(congress=115, chamber='house')[0]['members']
+	list_of_senate_members = get_list_of_members(congress=115, chamber='senate')[0]['members']
+
+	for house_member in list_of_house_members:
+		person_info = {
+			'first_name': house_member['first_name'], 
+			'last_name':house_member['last_name'],
+			'district': house_member['district'],
+			'state': house_member['state'],
+			'party': house_member['party'],
+			'type': 'house'
+			}
+		request_data['data']['house'].append(person_info)
+
+	for senate_member in list_of_senate_members:
+		person_info = {
+			'first_name': senate_member['first_name'], 
+			'last_name':senate_member['last_name'],
+			'district': None,
+			'state': senate_member['state'],
+			'party': senate_member['party'],
+			'type': 'senate'
+			}
+		request_data['data']['senate'].append(person_info)
+
+	return request_data
+
+def committee_info():
+	request_data = {
+		'table':'Committee', 
+		'data':{
+			'house_committees':[],
+			'senate_committees':[]
+			}
+		}
+
+	list_of_house_committees = get_committees(congress=115, chamber='house')[0]['committees']
+	list_of_senate_committees = get_committees(congress=115, chamber='senate')[0]['committees']
+
+	for committee in list_of_house_committees:
+		info = {
+			'name':committee['name'],
+			'chair':committee['chair']
+			}
+		request_data['data']['house_committees'].append(info)
+
+	for committee in list_of_senate_committees:
+		info = {
+			'name':committee['name'],
+			'chair':committee['chair']
+			}
+		request_data['data']['senate_committees'].append(info)
+
+	return request_data
+
+def bill_info():
+	request_data = {
+		'table':'Bill', 
+		'data':{
+			'house_committees':[],
+			'senate_committees':[]
+			}
+		}
+
+if __name__ == "__main__":
+
+	
