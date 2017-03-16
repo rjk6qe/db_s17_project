4717d29087074bed06fc41093508d6c08cb910c5
savleighm <savleighm@users.noreply.github.com>
2017-03-16 16:59:07 -0400

    added bill stuff

--- python/myexample.php
+++ python/myexample.php
@@ -38,4 +38,32 @@ function updateCongressperson(){
     $stmt->execute();
   }
 }
+
+function updateBill(){
+  $data = getJSONPostData()['data'];
+  $new_db = new DbUtil();
+  $stmt = $conn->prepare("INSERT INTO Bill (ID, member_id, title, committee_name) VALUES (?, ?, ?, ?)");
+  $stmt->bind_param("ssss", $bill_id, $member_id, $bill_title, $commitee_name);
+
+  $senator_votes = $data['senate_votes'];
+  $house__votes = $data['house_votes'];
+
+  // senate array
+  foreach ($senator_votes as $bill){
+    $bill_id = $bill['bill_id'];
+    $member_id = $bill['bill_sponsor_id'];
+    $bill_title = $bill['bill_title'];
+    $commitee_name = $bill['committee'];
+    $stmt->execute();
+  }
+  // house array
+  foreach ($house__votes as $bill){
+    $bill_id = $bill['bill_id'];
+    $member_id = $bill['bill_sponsor_id'];
+    $bill_title = $bill['bill_title'];
+    $commitee_name = $bill['committee'];
+    $stmt->execute();
+  }
 }
+
+?>
