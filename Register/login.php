742df9d54fca990defb49c3b8929815585a86050
fac3hc <fac3hc@labunix01.cs.virginia.edu>
2017-04-04 15:52:43 -0400

    changes

--- Register/login.php
+++ Register/login.php
@@ -1,59 +1,45 @@
-<?php
-/*require_once('db_conn.php');
-  echo "0";
-  $db =DbUtil::create();
-  $stmt = $db->prepare("select * from Constituent where username = ? AND password = ?)")
-  if(!stmt){
-        echo $db->error;
-  }
+<TYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-  $stmt->bind_param("ss", $username, $password);
-  $username = $_POST["inputUsername"];
-  $password = $_POST["inputPassword"];
+    <title>Follow Your Congressperson!</title>
 
-  if(!$stmt->execute()){
-        echo $stmt->error;
-  }
+    <?php
+								require_once('nav1.php');
+							       
+							      
 
-  $query->bind_result($result);
-  $query->fetch();
-  if($result){
-	echo "login succes";
-  }
+								?>
   
-  echo "login not successful"; */
-
-require_once('db_conn.php');
-  echo "0";
-  $db =DbUtil::create();
-  $stmt = $db->prepare("SELECT * FROM Constituent WHERE username = ? AND password = ?");
-  echo "1";
-  if(!$stmt){
-        echo $db->error;
-  }
-
-  echo "2";
-  $stmt->bind_param("ss", $username, $password);
-  $username = $_POST["inputUsername"];
-  $password = $_POST["inputPassword"];
-  
-  echo "Username is $username <br>";
-  echo "Password is $password <br>";
+</head>
 
 
-  if(!$stmt->execute()){
-        echo $stmt->error;
-  }
 
-  $stmt -> bind_result($user, $pass);
-  $stmt -> store_result();
-  if($stmt->num_rows == 1){
-   echo "result found! <br>";
-   header("Location: ../Main/main.html");
-  }else{
-   echo "result not found!";
-  }
-  //header("Location: ../index.html");
+<body>
 
-?>
+<nav class = "navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+<a class="navbar-brand" href="#">Welcome Back! Login here.</a>
+</nav>
 
+<div class="container">
+<form action="login_helper.php" method="post">
+ <div class='form-group'>
+  <label for="inputusername"> Username:</label>
+  <input type ="text" class="form-control" id="username" name="inputUsername">
+ </div>
+
+ <div class='form-group'>
+  <label for="pwd"> Password:</label>
+  <input type ="password" class="form-control" id="pwd" name="inputPassword">
+ </div>
+ <input type="submit" class="btn btn-default">
+</div>
+</body>
+</html>
